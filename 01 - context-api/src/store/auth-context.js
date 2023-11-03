7b3446c3f87df339ef3bf95d73b29ce6e354e87c
Lionel Kaneza <lionkaneza@gmail.com>
2023-11-03 12:47:46 +0200

    feat: manage all auth logics in context store[context]

--- 01 - context-api/src/store/auth-context.js
+++ 01 - context-api/src/store/auth-context.js
@@ -1,5 +1,39 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
-export default createContext({
+const AuthContext = createContext({
   isLoggedIn: false,
+  onLogin: (email, password) => {},
+  onLogout: () => {},
 });
+
+export const AuthContextProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn, onLogin: loginHandler, onLogout: logoutHandler }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;

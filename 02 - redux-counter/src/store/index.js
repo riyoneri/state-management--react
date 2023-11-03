4f939a16420747e5ad9aa7dcb53c6ef653e3bc22
Lionel Kaneza <lionkaneza@gmail.com>
2023-11-03 17:55:13 +0200

    refactor: refine incrementation logic in store[redux]

--- 02 - redux-counter/src/store/index.js
+++ 02 - redux-counter/src/store/index.js
@@ -3,13 +3,13 @@ import { createStore } from "redux";
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
     return {
-      counter: state.counter++,
+      counter: state.counter + 1,
     };
   }
 
   if (action.type === "decrement") {
     return {
-      counter: state.counter--,
+      counter: state.counter - 1,
     };
   }
 

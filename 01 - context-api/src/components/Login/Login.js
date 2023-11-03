315eeb06edf521cabb04d975c3d47f6d9726a6bf
Lionel Kaneza <lionkaneza@gmail.com>
2023-11-03 12:50:25 +0200

    refactor: use authentication from context store logic[context]

--- 01 - context-api/src/components/Login/Login.js
+++ 01 - context-api/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const Login = (props) => {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -10,6 +11,8 @@ const Login = (props) => {
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+
+  const autCtx = useContext(AuthContext)
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -59,7 +62,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    autCtx.onLogin(enteredEmail, enteredPassword);
   };
 
   return (

cb5bb2b4565360807d77e4164899f37e2fb994d8
Lionel Kaneza <lionkaneza@gmail.com>
2023-11-06 18:34:35 +0200

    feat: conditional render counter component[redux]

--- 02 - redux-counter/src/App.js
+++ 02 - redux-counter/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Header />
       {!isAuthenticated && <Auth />}
       {isAuthenticated && <UserProfile />}
-      <Counter />
+      {isAuthenticated && <Counter />}
     </>
   );
 }
